6c137ecc1fffdc790747c053885f720f3b03e524
Aniya <amallen046@gmail.com>
2021-09-02 15:15:36 -0500

    finished resposive design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,10 @@
-import React from 'react'
-import JobCard from './JobCard'
-import JobData from '../Data/data.json';
+import React from "react";
+import JobCard from "./JobCard";
+import JobData from "../Data/data.json";
 
 const Dashboard = () => {
-    const jobs = JobData.map(job => (
-        <JobCard key={job.id} job={job}/>
-    ))
-    return (
-        <div className="job-list">
-            {/* Job cards should be created in here */}
-            
-            {jobs}
-        </div>
-    )
-}
+  const jobs = JobData.map((job) => <JobCard key={job.id} job={job} />);
+  return <div className="job-list">{jobs}</div>;
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,34 +1,45 @@
-import React from 'react'
+import React from "react";
 
-const JobCard = ({job})=> {
-    return (
-        <div className="job-container">
-        {/* image, 
-        Header - name of company, new (boolean), featured (boolean)
-        Job title
-        Info - recently posted, job type, location
-        ---------------------------
-        tech stack
-         */}
-        <img src="../../images/account.svg" alt="Company Logo" />
-        <div>
-            <h5 className='company-name'> COMPANY NAME: {job.company}</h5>
-            <p className="new"> {job.new ? 'NEW!' : null}</p>
-            <p className="featured"> {job.featured ? 'FEATURED' : null}</p>
-            <h6>{job.position}</h6>
-            <ul>
-                <li>{job.postedAt}</li>
-                <li>{job.contract}</li>
-                <li>{job.location}</li>
+const JobCard = ({ job }) => {
+  return (
+    <div className="job-container">
+      <div className="left">
+        <div className="logo-div">
+          <img className="company-image" src={job.logo} alt="company logo" />
+        </div>
+
+        <div className="card-info">
+          <div className="card-header">
+            <h5 className="company-name">{job.company}</h5>
+            <p className="new"> {job.new ? "NEW!" : null}</p>
+            <p className="featured"> {job.featured ? "FEATURED" : null}</p>
+          </div>
+
+          <div>
+            <p className="job-position">{job.position}</p>
+            <ul className="job-info">
+              <li>{job.postedAt}</li>
+              <li>{job.contract}</li>
+              <li>{job.location}</li>
             </ul>
-            <br />
-            <div>
-                <p>Javascript</p>
-            </div>
+          </div>
         </div>
-            
-        </div>
-    )
-}
+      </div>
 
-export default JobCard
+      <div className="extra">
+        <p>{job.role}</p>
+        <p>{job.level}</p>
+
+        {job.languages.map((language, index) => (
+          <p key={index}>{language} </p>
+        ))}
+
+        {job.tools.map((tool, index) => (
+          <p key={index}>{tool}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
