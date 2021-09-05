f5d7159c8b58fa7de78734f405b26314f0636af3
Aniya <amallen046@gmail.com>
2021-09-04 20:33:47 -0500

    update x

--- README.md
+++ README.md
@@ -51,8 +51,6 @@ Users should be able to:
 
 ### What I learned
 
-
-
 **Note: Delete this note and the content within this section and replace with your own learnings.**
 
 ### Continued development
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import JobCard from "./JobCard";
 import JobData from "../Data/data.json";
 import Search from "./Search";
@@ -8,9 +8,8 @@ const Dashboard = () => {
   const [keywordArray, setKeywordArray] = useState([]);
   const [searchItem, setSearchItem] = useState("");
 
-  //   useEffect(() => {
-  //     addToSearch(searchItem);
-  //   }, [keywordArray])
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
 
   const addToSearch = (item) => {
     setClicked(true);
@@ -25,20 +24,79 @@ const Dashboard = () => {
 
     //item is the text you clicked
     setSearchItem(item);
+
+    //filter the search so certain jobcard can display
+    filterSearch();
   };
 
+  //TODO: FIXME
+  //Clears search bar
   const closeSearch = () => {
     setClicked(false);
     setKeywordArray([]);
   };
 
-  const jobs = JobData.map((job) => (
+  const filterSearch = () => {
+    //we want to filter out the jobs from jobdata
+    //if keyword doesnt exist in that job, the job should not be in the new array
+    //want to iterate through each object and check if they have any property key from keyword array
+
+    //this kinda works
+    // const matchingJobs = JobData.filter(({ role, level, languages, tools }) =>
+    //   keywordArray.includes(role, level, languages, tools)
+    // );
+
+    //setFilteredData(matchingJobs);
+    //console.log(matchingJobs);
+
+    let result = [];
+
+    result = allData.filter((job) => {
+      return job.role.search(searchItem) != -1;
+    });
+    setFilteredData(result);
+  };
+
+  //use effect hook
+  useEffect(() => {
+    setAllData(JobData);
+    setFilteredData(JobData);
+  }, []);
+
+  const jobs = filteredData.map((job) => (
     <JobCard key={job.id} job={job} clicked={addToSearch} />
   ));
+
+  // const jobItems = JobData.filter((job) => {
+  //   //if there is no search input yet
+  //   if (keywordArray.length === 0) {
+  //     console.log("empty");
+  //     return <JobCard key={job.id} job={job} clicked={addToSearch} />;
+
+  //   } else {
+  //     filterSearch();
+  //     console.log("WE IN HERE");
+  //   }
+  //   //map the filtered job array
+  // }).map((job) => {
+  //   return <JobCard key={job.id} job={job} clicked={addToSearch} />;
+  // });
+
+  // //overall, I think this is what i want to do
+  // if(keywordArray.length === 0) {
+  //   return <JobCard key={job.id} job={job} clicked={addToSearch} />;
+  // }else {
+  //   const jobsss = JobData.map(job => {
+  //     //map the filtered jobs
+  //     //but we should be able to rerender
+  //   })
+  // }
 
   return (
     <div className="job-list">
       {clicked ? <Search close={closeSearch} array={keywordArray} /> : null}
+
+      {/* {jobItems} */}
 
       {jobs}
     </div>
